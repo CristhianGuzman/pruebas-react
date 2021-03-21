87ea87dbad836566adac46f1287484f020b65b6f
unknown <cristhian.guzman@correounivalle.edu.co>
2021-03-21 13:48:44 -0500

    Pruebas react

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+//Cristian Guzman
+// 1630275-2711
+
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Contador from './Contador'
+import estilo from './index.css'
+
+
+const divRoot= document.querySelector('#root')
+ReactDOM.render(<Contador valorinicial={0}/>,divRoot)
